7d8716bebffc2df61762da7019fcb9394aea1e64
nafkhanzam <nafkhanalzamzami@gmail.com>
2023-02-22 04:06:58 +0700

    add optional args

--- pre-train/run_multitask_unified_pretraining.py
+++ pre-train/run_multitask_unified_pretraining.py
@@ -836,7 +836,7 @@ def evaluate(
     return result
 
 
-def main():
+def main(args=None):
     parser = argparse.ArgumentParser()
 
     # Required parameters
@@ -1096,7 +1096,8 @@ def main():
         "--freeze_decoder", action="store_true", help="Whether to freeze decoder of the modele",
     )
 
-    args = parser.parse_args()
+    if args is None:
+        args = parser.parse_args()
 
     if args.model_type in ["bert", "roberta", "distilbert", "camembert"] and not args.mlm:
         raise ValueError(
