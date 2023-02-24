fd11fd98b458227d9cde565ef393c796700211ed
nafkhanzam <nafkhanalzamzami@gmail.com>
2023-02-25 03:57:03 +0700

    update dataset

--- fine-tune/run.sh
+++ fine-tune/run.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=0
 RootDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RootDir=$RootDir/..
 
-Dataset=amrbart-test
+Dataset=amrbart
 DataPath=$RootDir/datasets/$Dataset
 
 Model=$1
