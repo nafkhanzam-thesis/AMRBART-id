de01364662bf1ffe17599693da077f226210082a
nafkhanzam <nafkhanalzamzami@gmail.com>
2023-03-24 11:11:07 +0700

    change masking in eval

--- setup-vm-ft.sh
+++ setup-vm-ft.sh
@@ -14,24 +14,34 @@ pushd models
   # tar -xvzf mbart-en-id-smaller-pre-trained.tar.gz
 
   #~ Fine-tuned model (mbart-en-id-smaller after 1 epoch)
-  epoch=16
-  wget https://storage.nafkhanzam.com/thesis/backups/mbart-large-50-finetuned-e$epoch.tar.gz
-  tar -xvzf mbart-large-50-finetuned-e$epoch.tar.gz
-
-  #~ Fine-tuned model (mbart-en-id-smaller after 1 epoch)
   # epoch=1
   # wget https://storage.nafkhanzam.com/thesis/backups/mbart-en-id-smaller-pre-trained-fine-tune-e$epoch.tar.gz
   # tar -xvzf mbart-en-id-smaller-pre-trained-fine-tune-e$epoch.tar.gz
+
+  #~ Fine-tuned model (mbart-en-id-smaller after 16 epoch)
+  # epoch=16
+  # wget https://storage.nafkhanzam.com/thesis/backups/mbart-large-50-finetuned-e$epoch.tar.gz
+  # tar -xvzf mbart-large-50-finetuned-e$epoch.tar.gz
+
+  #~ Pre-trained concat model (mbart-en-id-smaller)
+  wget https://storage.nafkhanzam.com/thesis/backups/mbart-en-id-smaller-concat-pre-trained.tar.gz
+  tar -xvzf mbart-en-id-smaller-concat-pre-trained.tar.gz
 popd
 
 mkdir datasets
 pushd datasets
-  wget https://storage.nafkhanzam.com/thesis/backups/amrbart-new.tar.gz
-  tar -xvzf amrbart-new.tar.gz
+  #~ AMRBART
+  # wget https://storage.nafkhanzam.com/thesis/backups/amrbart-new.tar.gz
+  # tar -xvzf amrbart-new.tar.gz
+
+  #~ Concat
+  wget https://storage.nafkhanzam.com/thesis/backups/amrbart-concat.tar.gz
+  tar -xvzf amrbart-concat.tar.gz
 popd
 
 pip install -r requirements.txt
 
+# ~ AMRBART
 # ./train.sh mbart-large-50-pretrained
 # ./eval.sh mbart-large-50-finetuned-e16
 
--- setup-vm-pt.sh
+++ setup-vm-pt.sh
@@ -3,25 +3,30 @@
 git clone https://github.com/nafkhanzam-thesis/AMRBART-v3
 cd AMRBART-v3
 
-sudo apt install git-lfs
-git lfs install
-
 mkdir models
 pushd models
   #~ mBART-large-50
-  git clone https://huggingface.co/facebook/mbart-large-50
+  # sudo apt install git-lfs
+  # git lfs install
+  # git clone https://huggingface.co/facebook/mbart-large-50
 
   #~ mBART-en-id
-  # wget https://storage.nafkhanzam.com/thesis/backups/mbart-en-id-smaller.tar.gz
-  # tar -xvzf mbart-en-id-smaller.tar.gz
+  wget https://storage.nafkhanzam.com/thesis/backups/mbart-en-id-smaller.tar.gz
+  tar -xvzf mbart-en-id-smaller.tar.gz
 popd
 
 mkdir datasets
 pushd datasets
+  #~ AMRBART
   wget https://storage.nafkhanzam.com/thesis/backups/amrbart-new.tar.gz
   tar -xvzf amrbart-new.tar.gz
+
+  #~ Concat
+  wget https://storage.nafkhanzam.com/thesis/backups/amrbart-concat.tar.gz
+  tar -xvzf amrbart-concat.tar.gz
 popd
 
 pip install -r requirements.txt
 
 # python main.py mbart-large-50 gpu [continue]
+# python main.py mbart-en-id-smaller gpu [continue]
