58290b02fe017b65138d4f674b93c174f340a589
nafkhanzam <nafkhanalzamzami@gmail.com>
2023-03-26 21:58:46 +0700

    update req

--- wikification/setup.sh
+++ wikification/setup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+# export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+# export CARGO_HTTP_MULTIPLEXING=false
+
 git clone https://github.com/nafkhanzam-thesis/AMRBART-v3
 cd AMRBART-v3/wikification
 
@@ -7,7 +12,9 @@ pip install -r requirements.txt
 
 git clone https://github.com/facebookresearch/BLINK.git
 pushd BLINK
-  pip install -r requirements.txt
+  # tail -n +2 requirements.txt > requirements.txt.output
+  # mv requirements.txt.output requirements.txt
+  # pip install -r requirements.txt
   chmod +x download_blink_models.sh
   ./download_blink_models.sh
 popd
