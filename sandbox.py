3473ceeb1891649113cb9ef107cb22c30b78c612
nafkhanzam <nafkhanalzamzami@gmail.com>
2023-04-24 21:42:14 +0700

    add paraphrase evaluation

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,45 @@
-from transformers import AutoModel
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, f1_score
+from amrlib.evaluate.smatch_enhanced import compute_smatch, get_entries
+from sklearn.metrics import roc_curve, roc_auc_score
 
-model = AutoModel.from_pretrained("models/mbart-large-50")
+entries = get_entries(f"/home/nafkhanzam/kode/nafkhanzam/thesis/AMRBART-v3/outputs/infer-mbart-en-id-smaller-pre-trained-fine-tune/val_outputs/test_generated_predictions_0.txt")
 
-pytorch_total_params = sum(p.numel() for p in model.parameters())
+ROWS = 50
 
-print("pytorch_total_params:", pytorch_total_params)
+results = []
+
+for i in range(ROWS):
+    _, _, f1 = compute_smatch([entries[i]], [entries[i+ROWS]])
+    results.append(f1)
+
+LABELS = ["NotEntail", "Entail_or_Paraphrase"]
+# max weighted f1: 0.476190
+# max acc: 0.463415
+# max macro f1: 0.464286
+pred = [LABELS[1] if x > 0.476190 else LABELS[0] for x in results]
+
+df = pd.read_csv(f"/home/nafkhanzam/kode/nafkhanzam/thesis/AMRBART-v3/datasets/wrete/WReTE-dev.csv")
+label = df["label"].to_list()
+
+report = classification_report(label, pred)
+print(report)
+
+print(confusion_matrix(label, pred, labels=LABELS))
+
+#~ Find ROC
+label_num = [1 if x == LABELS[1] else 0 for x in label]
+
+fpr,tpr,thresholds = roc_curve(label_num, results)
+scores = []
+for thres in thresholds:
+    y_pred = np.where(results > thres, 1, 0)
+    scores.append(f1_score(label_num, y_pred, average="weighted"))
+
+scores = pd.concat([pd.Series(thresholds), pd.Series(fpr), pd.Series(tpr), pd.Series(scores)],
+                        axis = 1)
+scores.columns = ['Thresholds', 'FPR', 'TPR', 'Score']
+scores.sort_values(by ='Score', ascending = False, inplace = True)
+scores.reset_index(drop = True,inplace = True)
+print(scores)
---
+++ sandbox2.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, f1_score
+from amrlib.evaluate.smatch_enhanced import compute_smatch, get_entries
+from sklearn.metrics import roc_curve, roc_auc_score
+
+#~ Penelitian Putra (2022)
+
+tn, fp, fn, tp = 9, 10, 3, 28
+precision = tp / (tp+fp)
+recall = tp / (tp+fn)
+f1 = 2*precision*recall/(precision+recall)
+accuracy = (tp+tn) / (tn+fp+fn+tp)
+
+print(accuracy, precision, recall, f1)
+
+tn, tp = tp, tn
+fn, fp = fp, fn
+precision = tp / (tp+fp)
+recall = tp / (tp+fn)
+f1 = 2*precision*recall/(precision+recall)
+accuracy = (tp+tn) / (tn+fp+fn+tp)
+
+print(accuracy, precision, recall, f1)
+
+# 0.74 0.7368421052631579 0.9032258064516129 0.8115942028985507
+# 0.74 0.75 0.47368421052631576 0.5806451612903226
+
+# >>> (0.8115942028985507+0.5806451612903226)/2
+# 0.6961196820944366 # macro f1
+# >>> (0.8115942028985507*31+0.5806451612903226*19)/50
+# 0.723833567087424 # weighted f1
